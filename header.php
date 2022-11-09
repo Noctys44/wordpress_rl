9393aff42cbcc6682819cebf8db7d46ccd4fc142
Cathy D <cathy.derocquencourt@gmail.com>
2022-11-09 16:38:05 +0100

    third commit

--- header.php
+++ header.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link href="css/style.css" rel="stylesheet">
     
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-    
-    <?php wp_body_open(); ?>
     <body <?php body_class(); ?>>
   <header class="header">
+    <ul>
+      <li><a href="">Blog</a></li>
     <a href="<?php echo home_url( '/' ); ?>">
       <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
     </a>  
+      <li><a href="">Contact</a></li>
+    </ul>
   </header>
