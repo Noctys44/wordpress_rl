d2f40f0d21acf55303622dead2f891d36b68ebc6
Cathy D <cathy.derocquencourt@gmail.com>
2022-11-09 16:16:13 +0100

    second commit

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link href="css/style.css" rel="stylesheet">
     
     <?php wp_head(); ?>
 </head>
